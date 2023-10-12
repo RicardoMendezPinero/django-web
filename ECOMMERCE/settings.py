5ced1a58c3efe7a8ab85ef25205221b1235380ff
RicardoMendezPinero <ricgabmendez2007@gmail.com>
2023-10-12 15:45:03 -0500

    n

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.moddleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ECOMMERCE.urls'
